fc15d7181d451877386036c5c0a5b083b233e1c5
Tarick Ahmed <newstartin702@gmail.com>
2024-08-12 19:12:35 -0700

    car menu commit

---
+++ index.js
@@ -0,0 +1,26 @@
+class car{
+constructor(make,model){
+    
+}
+this.make=make;
+this.model=model;
+}
+class Menu{
+constructor(){
+this.cars=[]
+
+}
+
+let car1=new Car("Honda,"Civic");{
+console.log(car1);
+
+
+
+}
+
+}
+
+
+
+
+}
---
+++ indexjs2.js
@@ -0,0 +1,90 @@
+class car{
+constructor(make,model){
+this.make=make
+this.model=model
+
+class Menu{
+constructor(){
+this.cars=[]
+
+let car1=new car("Honda," "Civic");{
+console.log(car1)
+addCar();{
+    let carMake= prompt(`Enter car make`)
+    let carModel=prompt (`Enter car model`)
+    newCar(carMake,carModel)
+sthis.cars.push(new car(carMake, carModel))
+    let menu=new menu()
+
+    viewCars(){
+for(let i=0;i<this.cars.length;i++
+    displayCars= $(this.cars[i].make) $(this.cars[i]).model)
+)
+
+alert(displayCars);
+
+alert{
+Car Inventory 
+
+
+menu.addCar();
+menu.addCar()
+menu.viewCars();
+menu.deleteCar();
+menu.viewCars();
+
+
+.start(){
+
+let selection =showMainMenu();
+
+while (selection i=0) {
+
+    switch(selection){
+
+        case"1": this.addCar();
+        break;
+
+        case"2" this.deleteCar();
+        break;
+        case"3" this.viewAllCars();
+        break;
+        default:selection=0
+
+        
+    }
+}
+selection= this.showMainMenu();
+alert("Exiting menu..goodbye")
+
+}
+
+
+
+}
+
+
+    }
+
+
+}
+
+
+
+}
+
+
+
+
+}
+
+
+}
+
+
+
+}
+
+
+
+}
